fa843db85c8d72cf7a0e1fcb48306b3409c5e356
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-11-08 23:03:33 +0800

    fix

--- lab/src/main/java/com/se/dto/FinishedReportDTO.java
+++ lab/src/main/java/com/se/dto/FinishedReportDTO.java
@@ -9,14 +9,14 @@ import java.sql.Date;
 public class FinishedReportDTO {
     private String labId;
     private String labName;
-    private Date endTime;
+//    private Date endTime;
     private Integer grades;
 
-    public FinishedReportDTO(String labId,String labName,Date endTime, Integer grades)
+    public FinishedReportDTO(String labId,String labName, Integer grades)
     {
         this.grades = grades;
         this.labId = labId;
         this.labName = labName;
-        this.endTime = endTime;
+//        this.endTime = endTime;
     }
 }
