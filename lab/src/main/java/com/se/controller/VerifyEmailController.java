936be4f79d0941c2508280b28e331b2bad824a1f
Zzy <74710014+Zhaobaba918@users.noreply.github.com>
2021-11-23 09:13:18 +0800

    Update VerifyEmailController.java

--- lab/src/main/java/com/se/controller/VerifyEmailController.java
+++ lab/src/main/java/com/se/controller/VerifyEmailController.java
@@ -2,6 +2,7 @@ package com.se.controller;
 
 import com.se.service.MailService;
 import com.se.util.VerifyCodes;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,34 @@ public class VerifyEmailController {
     MailService mailService;
 
     @PostMapping("/getCode")
-    public Boolean addActivity(@RequestParam String mail){ //传过来的json包装成实体
-        return mailService.sendSimpleMail(mail);
+    public Boolean addActivity(@RequestBody email mail){ //传过来的json包装成实体
+        return mailService.sendSimpleMail(mail.getEmail());
     }
 
     @PostMapping("/verify")
-    public Boolean verify(@RequestParam("email") String email, @RequestParam("code") String code){
-        return VerifyCodes.verify(email,code);
+    public Boolean verify(@RequestBody verifyCode verifyCode){
+        return VerifyCodes.verify(verifyCode.getEmail(),verifyCode.getCode());
+    }
+}
+
+@Data
+class email{
+    String email;
+    public email(String email) {
+        this.email = email;
+    }
+    public email() {
+    }
+}
+
+@Data
+class verifyCode{
+    String email;
+    String code;
+    public verifyCode(String email,String code) {
+        this.email = email;
+        this.code = code;
+    }
+    public verifyCode() {
     }
 }
