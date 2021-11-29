0f9c8322e7f4c87484b0d2877a3ae06c67b27a50
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-11-29 21:24:34 +0800

    fix :盖好了api

--- lab/src/main/java/com/se/controller/ComputeController.java
+++ lab/src/main/java/com/se/controller/ComputeController.java
@@ -4,10 +4,7 @@ import com.se.entity.Course;
 import com.se.service.ComputeService;
 import com.se.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -18,7 +15,7 @@ public class ComputeController {
     @Autowired
     private ComputeService computeService;
     @GetMapping("compute")
-    public ArrayList<ArrayList<Double>> ComputeRes (ArrayList<Double> list) {
+    public ArrayList<ArrayList<Double>> ComputeRes ( @RequestParam("list") ArrayList<Double> list) {
         return computeService.Compute(list);
     }
 }
