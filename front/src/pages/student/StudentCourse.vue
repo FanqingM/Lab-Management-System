493f6f6313b05fc4f2bbb95f04bb76edec95671b
user3984 <63094060+user3984@users.noreply.github.com>
2021-12-14 14:08:08 +0800

    Update StudentCourse.vue

--- front/src/pages/student/StudentCourse.vue
+++ front/src/pages/student/StudentCourse.vue
@@ -6,37 +6,24 @@
         <span><b>我的课程</b></span>
       </div>
       <el-main>
-        <div class="select_container">
+        <div class="card_container">
           <el-row :gutter="25">
             <el-col
-              :span="5"
-              v-for="(item, index) in r_fdnamelist"
-              :key="index"
-              class="ecol"
-            >
-              <el-checkbox @change="selectfd(item)" class="elcheckbox" border>{{
-                item.name
-              }}</el-checkbox>
-            </el-col>
-          </el-row>
-        </div>
-
-        <div class="card_container">
-          <el-row gutter="25">
-            <el-col
-              :span="5"
-              v-for="item in r_dnamelist"
-              :key="item.dname"
+              :span="6"
+              v-for="item in courses"
+              :key="item.courseId"
               class="ecol"
             >
               <el-card
                 :body-style="{ padding: '2px' }"
                 shadow="hover"
                 class="ecard"
-                @click="todoctor(item.id, item.dname)"
               >
+                <!-- @click="todoctor(item.id, item.dname)" -->
                 <div class="card_text">
-                  <font size="4" color="black"> {{ item.dname }}</font>
+                  <img src="https://edu-image.nosdn.127.net/6AE1A85C007E8E946EEC979BC9AB04A1.jpg?imageView&thumbnail=510y288&quality=100" width="100%"/>
+                  <p class="font-size:5"><b> {{ item.courseName }} </b></p>
+                  <p class="font-size:4"> {{ item.courseId }}</p>
                 </div>
               </el-card>
             </el-col>
@@ -59,139 +46,39 @@ export default {
     };
   },
   mounted() {
-    GETCoursesByStudentID({
-      studentId: store.state.id,
-      semeter: this.semester,
-      year: this.year,
-    })
-      .then((data) => {
-        console.log("data", data);
-        this.courses = data;
-      })
-      .catch((err) => {
-        console.log(err);
-        this.$message("课程信息获取错误");
+    for (let i = 0; i < 80; ++i) {
+      this.courses.push({
+        courseId: String(420343 + i),
+        courseName: "编译原理",
+        sectionId: "01",
+        semeter: true,
+        year: "2021-2022",
       });
+    }
+    // GETCoursesByStudentID({
+    //   studentId: store.state.id,
+    //   semeter: this.semester,
+    //   year: this.year,
+    // })
+    //   .then((data) => {
+    //     console.log("data", data);
+    //     this.courses = data;
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     this.$message("课程信息获取错误");
+    //   });
     this.loading = false;
   },
-  methods: {
-    selectfd(pra) {
-      if (target.checked) {
-        this.s_fdnamelist.push(pra);
-      } else {
-        var j;
-        for (var i = 0; i < this.s_fdnamelist.length; i++) {
-          if (this.s_fdnamelist[i].name == pra.name) j = i;
-        }
-        this.s_fdnamelist.splice(j, 1);
-      }
-      this.r_dnamelist.splice(0);
-
-      if (this.s_fdnamelist.length > 0) {
-        for (i = 0; i < this.dnamelist.length; i++)
-          for (j = 0; j < this.s_fdnamelist.length; j++)
-            if (this.dnamelist[i].fname == this.s_fdnamelist[j].name) {
-              this.r_dnamelist.push(this.dnamelist[i]);
-              continue;
-            }
-        // console.log('1');
-        // console.log(this.s_fdnamelist);
-        // console.log(this.dnamelist);
-        // console.log(this.fdnamelist);
-        // console.log(this.r_dnamelist);
-        // console.log(this.r_fdnamelist);
-      } else {
-        this.r_dnamelist.splice(0);
-        this.r_dnamelist = this.getobj.map((item) => {
-          return Object.assign(
-            {},
-            { dname: item.departmentName, fname: item.fatherDepartmentId }
-          );
-        });
-        // console.log('2');
-        // console.log(this.s_fdnamelist);
-        // console.log(this.dnamelist);
-        // console.log(this.fdnamelist);
-        // console.log(this.r_dnamelist);
-        // console.log(this.r_fdnamelist);
-      }
-
-      // console.log(pra.name);
-      // console.log(event.target.checked);
-    },
-    todoctor(id, dname) {
-      localStorage.setItem("SelectDepartmentId", id);
-      localStorage.setItem("SelectDepartmentName", dname);
-      console.log(id);
-      console.log(dname);
-      this.$router.push("/doctor");
-    },
-    initdata() {
-      fetch(this.url + this.urlid + "/all")
-        // 第一个 then 接受到的是请求头的相关信息
-        .then((res) => {
-          return res.json();
-        })
-        .then((res) => {
-          this.getobj = res;
-          this.dnamelist.splice(0);
-          this.fdnamelist.splice(0);
-
-          this.dnamelist = this.getobj.map((item) => {
-            return Object.assign(
-              {},
-              {
-                dname: item.departmentName,
-                fname: item.fatherDepartmentName,
-                id: item.departmentId,
-              }
-            );
-          });
-          this.r_dnamelist = this.getobj.map((item) => {
-            return Object.assign(
-              {},
-              {
-                dname: item.departmentName,
-                fname: item.fatherDepartmentName,
-                id: item.departmentId,
-              }
-            );
-          });
-
-          this.tempObj = this.getobj.map((item) => {
-            return Object.assign({}, { name: item.fatherDepartmentName });
-          });
-          for (var i = 0; i < this.tempObj.length; i++) {
-            for (var j = i + 1; j < this.tempObj.length; j++) {
-              if (this.tempObj[i].name === this.tempObj[j].name) {
-                this.tempObj.splice(j, 1);
-                j--;
-              }
-            }
-          }
-          this.fdnamelist = this.tempObj.map((item) => {
-            return Object.assign({}, { name: item.name });
-          });
-          this.r_fdnamelist = this.tempObj.map((item) => {
-            return Object.assign({}, { name: item.name });
-          });
-
-          // console.log(this.dnamelist);
-          // console.log(this.fdnamelist);
-        })
-        // 请求错误时执行
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style scoped>
 .ecard {
   border-radius: 7px;
-  background: #d7f0fa;
+  background: #ffffff;
+
   height: 80px;
   text-align: center;
 }
@@ -210,13 +97,12 @@ export default {
   background: white;
 }
 .elcheckbox:hover {
-  background: #a2d8ee;
+  background: #ffffff;
+  box-shadow: 0 25px 50px rgba(0, 0, 0, 0.5);
 }
 .card_container {
-  width: 80%;
+  width: 100%;
   position: relative;
-  left: 16%;
-  top: 0%;
   text-align: center;
 }
 .ecol {
@@ -236,5 +122,6 @@ export default {
 .el-card {
   height: 100%;
   border-radius: 15px;
+  overflow: auto;
 }
 </style>
